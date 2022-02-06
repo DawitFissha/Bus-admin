452f7dff3c83bd02b7765202fa4d8a70bc765d71
Dawit Fissha <dawitfissha1@gmail.com>
2022-02-06 11:21:33 +0300

    new component addButton added

---
+++ src/Components/addbutton.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import AddIcon from '@mui/icons-material/Add';
+import Button from '@mui/material/Button'
+export function AddButton({description}:{description:string}){
+    return (
+        <Button size = 'small' sx = {{
+            color:'white',
+            backgroundColor:'#007FFF',
+            marginLeft:'35%',
+            fontWeight:'bold',
+            // padding:'12px 24px',
+            borderRadius:'8px',
+            '&:hover': {
+                backgroundColor:'#0072E5',
+              }
+        }} startIcon={<AddIcon/>}>Add {description}</Button>
+    )
+}
