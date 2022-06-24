1dd22828a034a3963fea0b97a09f2d66981428f2
dawit fisseha <dawit.fisseha@CNETERP.COM>
2022-06-24 17:08:10 +0300

    today's changes ..

--- src/Components/cashierPage/index.tsx
+++ src/Components/cashierPage/index.tsx
@@ -10,12 +10,112 @@ import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
+import TextField,{TextFieldProps} from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import { AccountCircle } from '@mui/icons-material';
+import { alpha, styled } from '@mui/material/styles';
+import { OutlinedInputProps } from '@mui/material/OutlinedInput';
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import DirectionsIcon from '@mui/icons-material/Directions';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import DatePicker from '@mui/lab/DatePicker';
+
+function CustomizedInputBase() {
+  return (
+    <Paper
+      component="form"
+      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: '100%s',m:1,mt:2}}
+    >
+      <InputBase
+        sx={{ ml: 1}}
+        placeholder="Passenger Name"
+        inputProps={{ 'aria-label': 'search for a booking' }}
+      />
+      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+      <InputBase
+        sx={{ ml: 1,}}
+        placeholder="Passenger Phone"
+        inputProps={{ 'aria-label': 'search for a booking' }}
+      />
+      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+      <InputBase
+        sx={{ ml: 1,}}
+        placeholder="Passenger Seat"
+        inputProps={{ 'aria-label': 'search for a booking' }}
+      />
+      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+      <IconButton /*type="submit" */ sx={{ p: '10px' }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+   
+    </Paper>
+  );
+}
+
 interface TabPanelProps {
   children?: React.ReactNode;
   index: number;
   value: number;
 }
-
+// function TextFiveldForSearch(props:TextFieldProps) {
+//   return (
+//     <TextField
+//     {...props}
+//     size='small'
+//     sx={{ maxWidth: '250px',minWidth:'180px',borderRadius:"4px"}}
+//     InputProps={{
+//       startAdornment: (
+//         <InputAdornment position="start">
+//           <AccountCircle />
+//         </InputAdornment>
+//       ),
+//     }}
+    
+//   />
+//   )
+// }
+// const TextFieldForSearch = styled((props: TextFieldProps) => (
+//   <TextField
+//   size="small"
+//     sx={{ maxWidth: '250px',minWidth:'180px',borderRadius:"4px"}}
+//     InputProps={{ disableUnderline: true, 
+//       // endAdornment: (
+//       //   <InputAdornment position="start">
+//       //     <AccountCircle />
+//       //   </InputAdornment>
+//       // ),
+//     } as Partial<OutlinedInputProps>
+          
+//   }
+//     {...props}
+//   />
+// ))(({ theme }) => ({
+//   '& .MuiFilledInput-root': {
+//     border: '1px solid #e2e2e1',
+//     overflow: 'hidden',
+//     borderRadius: '32px',
+//     backgroundColor: theme.palette.mode === 'light' ? '#fcfcfb' : '#2b2b2b',
+//     transition: theme.transitions.create([
+//       'border-color',
+//       'background-color',
+//       'box-shadow',
+//     ]),
+//     '&:hover': {
+//       backgroundColor: 'transparent',
+//     },
+//     '&.Mui-focused': {
+//       backgroundColor: 'transparent',
+//       boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 2px`,
+//       borderColor: theme.palette.primary.main,
+//     },
+//   },
+// }));
 function TabPanel(props: TabPanelProps) {
   const { children, value, index, ...other } = props;
 
@@ -36,7 +136,7 @@ function TabPanel(props: TabPanelProps) {
   );
 }
 
-function a11yProps(index: number) {
+function a11yProps(index:number) {
   return {
     id: `simple-tab-${index}`,
     'aria-controls': `simple-tabpanel-${index}`,
@@ -49,7 +149,7 @@ export default function MainCashierPage() {
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
-
+  const [dateValue, setDateValue] = React.useState<Date | null>(null);
   return (
     <Box sx={{ width: '100%' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
@@ -66,14 +166,16 @@ export default function MainCashierPage() {
                 <Box sx={{flexGrow:1}}>
                 <BookingHistory/>
                 </Box>
-                <Box>
+                <Box sx={{ml:2}}>
                 <div
                 style={{
                     width:'600px',
                     height:'200px',
                     backgroundColor:'white',
+                    borderLeft:"8px solid green"
                 }}
                 >
+           <Box sx={{display:'flex',gap:'15px',flexDirection:'column'}}>
          <Box sx={{display:'flex',gap:'15px',alignItems:'center'}}>
         <Box>
       <FormControl size='small' sx={{ maxWidth: '250px',minWidth:'180px',m:1}}>
@@ -92,7 +194,35 @@ export default function MainCashierPage() {
     <Box>
         From A to B departing on 2022-05-11 at 8:30 pm from mexico
     </Box>
-    </Box>           
+    </Box>    
+    </Box>      
+
+    {/* <Box sx={{display:'flex',gap:'7px',alignItems:'center',m:1}}>
+      <Box>
+      <TextFieldForSearch
+        id="search-by-passenger-name"
+        label="passenger name"
+        />
+      </Box>
+      <Box>
+        phone number
+      </Box>
+      <Box>
+        seat number
+      </Box>
+      </Box>         */}
+      <CustomizedInputBase/>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <DatePicker
+        
+        label="Booking Date"
+        value={dateValue}
+        onChange={(newValue) => {
+          setDateValue(newValue);
+        }}
+        renderInput={(params) => <TextField  sx={{ maxWidth: '250px',minWidth:'180px',m:1}} {...params} />}
+      />
+    </LocalizationProvider>
                 </div>
                 </Box>
         </Box>
